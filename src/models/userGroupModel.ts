2f04f8db4a2c1d8840d63aa2c946697b1ed8488a
Yaroslav Bushmakin <yaroslav_bushmakin@epam.com>
2020-02-05 14:00:25 +0200

    Added many-to-many relation between user and group

--- src/models/userGroupModel.ts
+++ src/models/userGroupModel.ts
@@ -1,5 +1,7 @@
 import { Model, DataTypes } from 'sequelize';
 import { sequelizeDB } from '../data-access';
+import { GroupModel } from './groupModel';
+import { UserModel } from './userModel';
 
 export class UserGroupModel extends Model {
     public userId!: string;
@@ -7,9 +9,24 @@ export class UserGroupModel extends Model {
 }
 
 UserGroupModel.init({
-    userId: DataTypes.STRING,
-    groupId: DataTypes.STRING
+    userId: {
+        type: DataTypes.STRING,
+        references: {
+            model: UserModel,
+            key: 'id'
+        }
+    },
+    groupId: {
+        type: DataTypes.STRING,
+        references: {
+            model: GroupModel,
+            key: 'id'
+        }
+    },
 }, {
     tableName: 'user_group',
     sequelize: sequelizeDB
 });
+
+UserModel.belongsToMany(GroupModel, { through: UserGroupModel, foreignKey: 'userId', otherKey: 'groupId' });
+GroupModel.belongsToMany(UserModel, { through: UserGroupModel, foreignKey: 'groupId', otherKey: 'userId' });
